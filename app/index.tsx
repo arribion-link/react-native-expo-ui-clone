b4754c19cb39d277b556332e5dfa2aa07f2bae2c
Dev Jeff <arribion@gmail.com>
2026-01-05 11:31:41 +0300

    updated the UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Text, TextInput, View, Image } from "react-native";
-
+import { Camera, UserCheck,ShieldBan, Clipboard, PillBottle } from "lucide-react-native";
+// import { UserCheck } from "lucide-react" 
 export default function Index() {
   return (
     <View className="text-blue-600 m-2">
@@ -21,8 +22,51 @@ export default function Index() {
       </View>
       <View className="mt-8">
         <Text className="font-bold text-3xl">Services</Text>
+        <View className="flex-row justify-start mt-12">
+          <View className="border rounded-2xl p-2 m-1 w-fit">
+            <UserCheck color="blue" size={48} />
+          </View>
+          <View className="border rounded-2xl p-2 m-1 w-fit">
+            <ShieldBan color="blue" size={48} />
+          </View>
+          <View className="border rounded-2xl p-2 m-1 w-fit">
+            <PillBottle color="blue" size={48} />
+          </View>
+          <View className="border rounded-2xl p-2 m-1 w-fit">
+            <Clipboard color="blue" size={48} />
+          </View>
+          <View className="border rounded-2xl p-2 m-1 w-fit">
+            <PillBottle color="blue" size={48} />
+          </View>
+          <View className="border rounded-2xl p-2 m-1 w-fit">
+            <Clipboard color="blue" size={48} />
+          </View>
+        </View>
+      </View>
+
+      <View className="flex-row gap-12 bg-slate-400 rounded-2xl mt-12">
         <View>
-          <View></View>
+          <Text className="text-blue-500 text-4xl">Get the best</Text>
+          <Text className="text-blue-500 text-4xl"> medical Services</Text>
+        </View>
+        <View>
+          <Image
+            source={{
+              uri: "https://images.unsplash.com/photo-1622253692010-333f2da6031d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9jdG9yfGVufDB8fDB8fHww",
+            }}
+            style={{
+              width: 200,
+              height: 200,
+            }}
+            className="rounded-2xl"
+          />
+        </View>
+      </View>
+
+      <View>
+        <Text className="text-3xl font-bold mt-12">Upcoming Appointments</Text>
+        <View>
+          
         </View>
       </View>
     </View>
